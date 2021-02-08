bfa33be09693067c51c3d0f05abbe0b039e37f1f
krucx <55143359+krucx@users.noreply.github.com>
2021-02-08 12:40:54 +0530

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -1,49 +1,76 @@
 import cv2
 import numpy as np
+import random
 
-X = []
-Y = []
+def preprocess():
+	X = []
+	Y = []
 
-for i in range(1,462):
+	for i in range(1,462):
 
-	img = cv2.imread('images/img_'+str(i)+'.jpg')
-	#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		img = cv2.imread('images/img_'+str(i)+'.jpg')
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-	# Removing noise
-	#img = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
-	
-	x_s = 512/img.shape[1]
-	y_s = 512/img.shape[0]
+		#Removing noise
+		#img = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+		
+		x_s = 512/img.shape[1]
+		y_s = 512/img.shape[0]
 
-	resized_img = cv2.resize(img,(512,512))
+		resized_img = cv2.resize(img,(512,512))
 
-	file = "ground_truth/gt_img_"+str(i)+".txt"
+		file = "ground_truth/gt_img_"+str(i)+".txt"
 
-	labels = np.zeros((16,16,5))
-	
-	with open(file,'r') as f:
-		for row in f:
-			line = row.split(',')
-			x1 = int(int(line[0].strip())*x_s)
-			y1 = int(int(line[1].strip())*y_s)
-			x2 = int(int(line[2].strip())*x_s)
-			y2 = int(int(line[3].strip())*y_s)
-			center_x = (x1+x2)//2
-			center_y = (y1+y2)//2
-			w = abs(x2-x1)
-			h = abs(y2-y1)
-			l = np.array([1,center_x,center_y,w,h])
-			labels[center_y//32][center_x//32] = l
-			
+		labels = np.zeros((16,16,5))
+		
+		with open(file,'r') as f:
+			for row in f:
+				if row.count(',') > 1:
+					line = row.split(',')
+				else:
+					line = row.split()
+				x1 = int(int(line[0].strip())*x_s)
+				y1 = int(int(line[1].strip())*y_s)
+				x2 = int(int(line[2].strip())*x_s)
+				y2 = int(int(line[3].strip())*y_s)
+				center_x = (x1+x2)/2
+				center_y = (y1+y2)/2
+				w = abs(x2-x1)
+				h = abs(y2-y1)
+				l = np.array([1,center_x,center_y,w,h])
+				labels[int(center_y)//32][int(center_x)//32] = l
+				
 
-			#cv2.rectangle(resized_img,(int(x*x1),int(y*y1)),(int(x*x2),int(y*y2)),(255,0,0))
-			#cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0))
+				#cv2.rectangle(resized_img,(int(x*x1),int(y*y1)),(int(x*x2),int(y*y2)),(255,0,0))
+				#cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0))
 
-	X.append(resized_img)
-	Y.append(labels)
 
-X = np.asarray(X)
-print(X.shape)
-Y = np.asarray(Y)
-print(Y.shape)
-	
+		X.append(resized_img)
+		Y.append(labels)
+		print(i)
+
+	X = np.asarray(X)
+	print(X.shape)
+	Y = np.asarray(Y)
+	print(Y.shape)
+	return X,Y
+
+def show_random(X,Y):
+
+	idx = random.randint(0,X.shape[0]-1)
+	img = cv2.cvtColor(X[idx],cv2.COLOR_RGB2BGR)
+	for i in range(Y.shape[1]):
+		for j in range(Y.shape[2]):
+			if Y[idx][i][j][0]==1:
+				x1 = Y[idx][i][j][1] - Y[idx][i][j][3]/2
+				x2 = Y[idx][i][j][1] + Y[idx][i][j][3]/2
+				y1 = Y[idx][i][j][2] - Y[idx][i][j][4]/2
+				y2 = Y[idx][i][j][2] + Y[idx][i][j][4]/2
+				cv2.rectangle(img,(int(x1),int(y1)),(int(x2),int(y2)),(255,0,0))
+	cv2.imshow('Image',img)
+	cv2.waitKey(0)
+
+X,Y = preprocess()
+for i in range(5):
+	show_random(X,Y)
+
